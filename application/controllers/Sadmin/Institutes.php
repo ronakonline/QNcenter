fe64cacae6f31a28428501d8b6dcfed7d703cec2
Ronak <ronakonline1@gmail.com>
2020-05-26 08:53:10 +0530

    All Institute Action Done in SAdmin

--- application/controllers/Sadmin/Institutes.php
+++ application/controllers/Sadmin/Institutes.php
@@ -15,4 +15,32 @@ class Institutes extends  CI_Controller{
 		$data['institutes'] = $this->InstitutesM->allinstitutes();
 		$this->load->view('Sadmin/index',$data);
 	}
+
+	public function action($fun, $id){
+		check_sadmin_login_status();
+		if($fun=='verify'){
+			$op = $this->InstitutesM->verify($id);
+			if($op){
+				$_SESSION['success']="Account Verified";
+			}else{
+				$_SESSION['error']="Error Occurred";
+			}
+
+		}elseif ($fun=='delete'){
+			$op = $this->InstitutesM->delete($id);
+			if($op){
+				$_SESSION['success']="Account Deleted";
+			}else{
+				$_SESSION['error']="Error Occurred";
+			}
+		}elseif ($fun=='ban'){
+			$op = $this->InstitutesM->ban($id);
+			if($op){
+				$_SESSION['success']="Account Banned";
+			}else{
+				$_SESSION['error']="Error Occurred";
+			}
+		}
+		redirect(base_url('Sadmin/Institutes'));
+	}
 }
